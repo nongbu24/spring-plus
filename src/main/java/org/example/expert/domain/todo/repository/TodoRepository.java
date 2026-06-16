c15b44f4e57b23db249b03154e0f205b79502e19
nongbu24 <pop524@naver.com>
2026-06-16 21:12:26 +0900

    feat: Level. 2-8

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -8,11 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRepository {
 
-    // 검색 조건이 null이면 해당 조건을 건너뛰고, 값이 있으면 날씨와 수정일 범위로 필터링합니다.
     @Query(value = "SELECT t FROM Todo t " +
             "LEFT JOIN FETCH t.user u " +
             "WHERE (:weather IS NULL OR t.weather = :weather) " +
@@ -29,9 +27,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             @Param("endDateTime") LocalDateTime endDateTime,
             Pageable pageable
     );
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
